527ea135fa5a728fb7364280b90c9888a8ed06d8
IvanGyulumyan99 <ivan.gyulumyan@gmail.com>
2019-12-21 01:14:29 +0400

    third commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Second hello worldg");
+        System.out.println("third commit on master branch");
     }
 }
